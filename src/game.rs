512d80d42495e0d9ace76885d9eef3739b5fe012
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-06-05 22:21:31 +0200

    First successful parse of a full game with no variations and no comments

--- src/game.rs
+++ src/game.rs
@@ -1,18 +1,50 @@
 
+#[derive(Debug)]
+pub struct Comment {
+	pub text: String,
+	pub tags: Vec<(String, String)>
+}
+
+impl Comment {
+	pub fn new() -> Comment {
+		Comment {
+			text: String::new(),
+			tags: Vec::new()
+		}
+	}
+}
+
+#[derive(Debug)]
 pub struct Game {
-    pub game_move: String,
-    pub main_line: Option<Game>,
-    pub variations: Vec<Game>,
-    pub comments: Vec<String>
+	pub game_move: String,
+	pub main_line_next: Option<Box<Game>>,
+	pub variations: Vec<Game>,
+	pub comment: Comment
 }
 
 impl Game {
-    pub fn new() -> Game {
-        Game {
-            game_move: "".to_string(),
-            main_line: None,
-            variations: Vec::new(),
-            comments: Vec::new()
-        }
-    }
+	pub fn new() -> Game {
+		Game {
+			game_move: "".to_string(),
+			main_line_next: None,
+			variations: Vec::new(),
+			comment: Comment::new()
+		}
+	}
+
+	pub fn set_move_text(&mut self, text: String) {
+		self.game_move = text;
+	}
+
+	pub fn set_next_move(&mut self, game: Game) {
+		self.main_line_next = Some(Box::new(game));
+	}
+
+	pub fn add_variation(&mut self, variation: Game) {
+		self.variations.push(variation);
+	}
+
+	pub fn set_comment(&mut self, comment: Comment) {
+		self.comment = comment;
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::io::{BufRead, Write, Result};
+use std::io::{BufRead};
 
 use regex::Regex;
+
+mod game;
 
 #[derive(Debug,PartialEq)]
 enum CharacterType {
@@ -17,13 +19,16 @@ enum CharacterType {
 enum Side { White, Black }
 
 #[derive(Debug)]
-enum StringType {
+enum ResultType { White, Draw, Black }
+
+#[derive(Debug)]
+enum TokenType {
 	VariantDelim { open: bool },
 	CommentDelim { open: bool },
 	TagDelim { open: bool },
 	MoveNumber { id: u32, side: Side },
-	MoveText,
-	Other
+	Text,
+	Result { result: ResultType }
 }
 
 fn classify_char(c: char) -> CharacterType {
@@ -42,12 +47,12 @@ fn classify_char(c: char) -> CharacterType {
 	}
 }
 
-type TokenizedPGN = Vec<(String, StringType)>;
+type TokenizedPGN = Vec<(String, TokenType)>;
 
-fn is_move_number(str: &String) -> Option<StringType> {
+fn is_move_number(str: &String) -> Option<TokenType> {
 	let re = Regex::new(r"^(?<move_number>[0-9]+)(?<side>\.+)$").unwrap();
 	if let Some(caps) = re.captures(str) {
-		Some(StringType::MoveNumber{
+		Some(TokenType::MoveNumber{
 			id: caps["move_number"].parse::<u32>().unwrap(),
 			side: if caps["side"] == ".".to_string() { Side::White } else { Side::Black }
 		})
@@ -57,6 +62,24 @@ fn is_move_number(str: &String) -> Option<StringType> {
 	}
 }
 
+fn is_result_tag(str: &String) -> Option<TokenType> {
+	if str.contains("-") {
+		if str == "1-0" {
+			return Some(TokenType::Result { result: ResultType::White });
+		}
+		else if str == "1/2-1/2" {
+			return Some(TokenType::Result { result: ResultType::Draw });
+		}
+		else if str == "0-1" {
+			return Some(TokenType::Result { result: ResultType::Black });
+		}
+		else {
+			return None;
+		}
+	}
+	None
+}
+
 fn add_token(str: String, res: &mut TokenizedPGN) {
 	println!("Adding string '{str}'");
 
@@ -64,8 +87,12 @@ fn add_token(str: String, res: &mut TokenizedPGN) {
 		res.push( (str, move_number) );
 		return;
 	}
+	if let Some(result) = is_result_tag(&str) {
+		res.push( (str, result) );
+		return;
+	}
 	
-	res.push( (str, StringType::Other) );
+	res.push( (str, TokenType::Text) );
 }
 
 fn tokenize(s: String) -> TokenizedPGN {
@@ -86,32 +113,94 @@ fn tokenize(s: String) -> TokenizedPGN {
 				if next_str != "".to_string() {
 					add_token(next_str, &mut res);
 				}
-				res.push((c.to_string(), StringType::VariantDelim{open: o} ));
+				res.push((c.to_string(), TokenType::VariantDelim{open: o} ));
 				next_str = "".to_string();
 			},
 			CharacterType::CurlyBracket(o) => {
 				if next_str != "".to_string() {
 					add_token(next_str, &mut res);
 				}
-				res.push((c.to_string(), StringType::CommentDelim{open: o} ));
+				res.push((c.to_string(), TokenType::CommentDelim{open: o} ));
 				next_str = "".to_string();
 			},
 			CharacterType::SquareBracket(o) => {
 				if next_str != "".to_string() {
 					add_token(next_str, &mut res);
 				}
-				res.push((c.to_string(), StringType::TagDelim{open: o} ));
+				res.push((c.to_string(), TokenType::TagDelim{open: o} ));
 				next_str = "".to_string();
 			}
 		}
 	}
-	res.push((next_str, StringType::Other));
+	add_token(next_str, &mut res);
 
 	res
 }
 
+fn parse_comment(data: &TokenizedPGN, i: usize) -> (game::Comment, usize) {
+	(game::Comment::new(), i + 1)
+}
+
+fn build_variation(data: &TokenizedPGN, i: usize) -> (game::Game, usize) {
+	(game::Game::new(), i + 1)
+}
+
+fn build_pgn_tree(data: &TokenizedPGN, mut i: usize) -> (Option<game::Game>, usize) {
+	let mut g = game::Game::new();
+	let total_length = data.len();
+	
+	if i >= total_length {
+		return (None, i);
+	}
+
+	match &data[i].1 {
+		TokenType::MoveNumber { id, side } => {
+			
+			let (result, next) = build_pgn_tree(&data, i + 1);
+			if let Some(rest) = result {
+				g = rest;
+			}
+			i = next;
+		},
+
+		TokenType::Text => {
+			g.set_move_text(data[i].0.clone());
+			
+			let (result, next) = build_pgn_tree(&data, i + 1);
+			if let Some(rest) = result {
+				g.set_next_move(rest);
+			}
+			i = next;
+		},
+
+		TokenType::CommentDelim { open: o } => {
+			if *o == false {
+				panic!("Unexpected closed comment delimiter at token {i}");
+			}
+
+			let (com, next) = parse_comment(&data, i + 1);
+				g.set_comment(com);
+			i = next;
+		},
+
+		TokenType::VariantDelim { open: o } => {
+			if *o == false {
+				panic!("Unexpected closed variation delimiter at token {i}");
+			}
+
+			let (var, next) = build_variation(&data, 3);
+			g.add_variation(var);
+			i = next;
+		},
+		
+		_ => { }
+	}
+
+	(Some(g), 0)
+}
+
 fn main() {
-	let p = "sample_games/fg_01.pgn";
+	let p = "sample_games/fg_01_nv_nc.pgn";
 	let mut entire_file_str = String::new();
 
 	let file = std::fs::File::open(p).expect("Failed to open file");
@@ -121,7 +210,11 @@ fn main() {
 	}
 
 	let res = tokenize(entire_file_str);
-	for str in res {
-		println!("'{str:?}'");
+	for str in res.iter() {
+		println!("{:?} -- {:?}", str.0, str.1);
+	}
+
+	if let (Some(game), i) = build_pgn_tree(&res, 0) {
+		println!("{:#?}", game);
 	}
 }
